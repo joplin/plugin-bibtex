0aa123e47bbc3a0cad5dcf09b60aa574abbffa93
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-06-14 01:05:11 +0200

    test: Move bib test data into a separate file & Include more bibtex entries

--- test/parser.util.test.ts
+++ test/parser.util.test.ts
@@ -1,30 +1,15 @@
+import { readFileSync, writeFileSync } from "fs";
 import { parse } from "../src/util/parser.util";
+import { join } from "path";
 
 describe("Parser", () => {
 
     it("Correct Parsing", () => {
-        let raw = `
-        @article{Steinbeck2003,
-            author = {Steinbeck, Christoph and Han, Yongquan and Kuhn, Stefan and Horlacher, Oliver and Luttmann, Edgar and Willighagen, Egon},
-            year = {2003},
-            month = {10},
-            title = {{The Chemistry Development Kit (CDK): an open-source Java library for Chemo- and Bioinformatics.}},
-            journal = {Journal of chemical information and computer sciences},
-            volume = {43},
-            number = {2},
-            pages = {493--500},
-            doi = {10.1021/ci025584y},
-            isbn = {2214707786},
-            issn = {0095-2338},
-            pmid = {12653513},
-            url = {http://www.ncbi.nlm.nih.gov/pubmed/12653513}
-          }
-        `;
-
+        let raw = readFileSync(join(__dirname, "assets", "test.bib"), "utf-8");
         let result = parse(raw);
 
         // Validation
-        expect(result.length).toBe(1);              // The result is an array of reference objects
+        expect(result).toBeInstanceOf(Array);             // The result is an array of reference objects
         
         let firstItem = result[0];
         expect(firstItem).toHaveProperty("id", "Steinbeck2003");
