09482305f70208032875d71c3229076edaf6bd7b
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-06-17 15:38:52 +0200

    feat: Parse the bibtex file and store results upon executing the command

--- src/add-bibtex-reference.command.ts
+++ src/add-bibtex-reference.command.ts
@@ -1,6 +1,12 @@
 import joplin from "api";
 import constants from "./constants";
 import { showCitationPopup } from './ui/citation-popup';
+import { Reference } from "./model/reference.model";
+import { parse } from "./util/parser.util";
+import { DataStore } from "./data/data-store";
+const fs = joplin.require("fs-extra");
+
+const showMessageBox = joplin.views.dialogs.showMessageBox;
 
 /**
  * Register the main command of the plugin
@@ -11,7 +17,49 @@ export async function registerAddBibTexReferenceCommand () {
         label: 'Add BibTeX Reference',
         iconName: constants.PLUGIN_ICON,
         execute: async () => {
-            await showCitationPopup();
+
+            // Get file Path and check for errors
+            const filePath: string = await joplin.settings.value(constants.SETTINGS_FILE_PATH_ID);
+            if (!isFileExisting(filePath)) {
+                await showMessageBox(constants.ERROR_FILE_NOT_FOUND);
+                return;
+            }
+            if (!isFileReadable(filePath)) {
+                await showMessageBox(constants.ERROR_PERMISSION_DENIED);
+                return;
+            }            
+
+            // Import bibtex raw data
+            const fileContent: string = await fs.readFile(filePath, "utf8");            
+
+            try {
+
+                // Parse the raw data and store it
+                const refs: Reference[] = parse(fileContent);
+                DataStore.setReferences(refs);
+
+                // Show the citation popup
+                await showCitationPopup();
+
+            } catch (e) {
+                console.log(e.message);
+                await showMessageBox(constants.ERROR_PARSING_FAILED);
+            }
+
         }
     });
+}
+
+function isFileExisting (file: string): boolean {
+    try {
+        fs.accessSync(file, fs.constants.F_OK | fs.constants.R_OK);
+        return true;
+    } catch (e) { return false; }
+}
+
+function isFileReadable (file: string): boolean {
+    try {
+        fs.accessSync(file, fs.constants.R_OK);
+        return true;
+    } catch (e) { return false; }
 }
--- src/constants.ts
+++ src/constants.ts
@@ -7,5 +7,8 @@ export default {
     "SETTINGS_FILE_PATH_ID": "bibtex.file",
     "ADD_BIBTEX_REFERENCE_COMMAND": "addBibTeXReference",
     "PLUGIN_TOOLBAR_BUTTON_ID": "bibtex_plugin_toolbar_button",
-    "CITATION_POPUP_ID": "citation_popup"
+    "CITATION_POPUP_ID": "citation_popup",
+    "ERROR_FILE_NOT_FOUND": "Error: File not found",
+    "ERROR_PERMISSION_DENIED": "Error: Read Permission is denied on the .bib file",
+    "ERROR_PARSING_FAILED": "Error: Something wrong happened while parsing"
 };
