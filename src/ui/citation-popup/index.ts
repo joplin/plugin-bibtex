dbc1777fdf8d572f42fc4ba87fe94412dfcb9fe7
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-06-17 18:02:35 +0200

    feat(Citation Popup): Enable the citation popup to show the list of references

--- src/ui/citation-popup/index.ts
+++ src/ui/citation-popup/index.ts
@@ -1,23 +1,42 @@
 import joplin from "api";
+import { DataStore } from "../../data/data-store";
 import constants from "../../constants";
+import { Reference } from "../../model/reference.model";
 const fs = joplin.require("fs-extra");
+
+let popupHandle: string = "";
 
 /**
  * Show a dialog for the user to choose from a list of references
  * to be inserted in the note content
  */
-
-let popupHandle: string = "";
-
 export async function showCitationPopup () {
 
     // If the dialog was not initialized, create it and get its handle
-    if (popupHandle === "")
+    if (popupHandle === "") {
         popupHandle = await joplin.views.dialogs.create(constants.CITATION_POPUP_ID);
-    
-    const installDir = await joplin.plugins.installationDir();
-    const html = await fs.readFile(installDir + '/ui/citation-popup/view.html', 'utf8');
+    }
+
+    let html: string = await fs.readFile(
+        await joplin.plugins.installationDir() + "/ui/citation-popup/view.html",
+        'utf8'
+    );
+
+    const refs: Reference[] = DataStore.getAllReferences();
+    html = html.replace("<!-- content -->", fromRefsToHTML(refs));
 
     await joplin.views.dialogs.setHtml(popupHandle, html);
     await joplin.views.dialogs.open(popupHandle);
+}
+
+function fromRefsToHTML (refs: Reference[]): string {
+    const ans: string = (
+        `<ul>` +
+            refs
+                .map(ref => `<li>${ref.title}</li>`)
+                .reduce((acc, curr) => acc + curr) +
+        `</ul>`
+    );
+    console.log(ans);
+    return ans;
 }
