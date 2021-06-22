8d3991455d2cee812381ca66dc5a016bb597c582
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-06-22 12:14:59 +0200

    perf: Allow tests to run in parallel

--- test/data-store.test.ts
+++ test/data-store.test.ts
@@ -1,12 +1,14 @@
 import { DataStore } from "../src/data/data-store";
 import { Reference } from "../src/model/reference.model";
-import { readFileSync } from "fs";
+import { promises as fs } from "fs";
 import { join } from "path";
 
 describe("Data Store", () => {
 
-    it("Calling SetReference() updates the data in the store", () => {
-        const data: Reference[] = JSON.parse(readFileSync(join(__dirname, "assets", "test.json"), "utf-8"));
+    it("Calling SetReference() updates the data in the store", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
         DataStore.setReferences(data);
 
         const res: Reference[] = DataStore.getAllReferences();
@@ -17,8 +19,10 @@ describe("Data Store", () => {
         expect(res).toEqual(data);
     });
 
-    it("getReferenceById() returns the reference that matches the id", () => {
-        const data: Reference[] = JSON.parse(readFileSync(join(__dirname, "assets", "test.json"), "utf-8"));
+    it("getReferenceById() returns the reference that matches the id", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
         DataStore.setReferences(data);
 
         const refId = DataStore.getAllReferences()[0].id;
@@ -27,8 +31,10 @@ describe("Data Store", () => {
         expect(res).toEqual(DataStore.getAllReferences()[0]);
     });
 
-    it("getReferenceById() throws error when the reference is not found", () => {
-        const data: Reference[] = JSON.parse(readFileSync(join(__dirname, "assets", "test.json"), "utf-8"));
+    it("getReferenceById() throws error when the reference is not found", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
         DataStore.setReferences(data);
 
         const refId = "someRandomId";
--- test/format-ref.test.ts
+++ test/format-ref.test.ts
@@ -1,29 +1,33 @@
 import { formatReference } from "../src/util/format-ref.util";
 import { Reference } from "../src/model/reference.model";
-import { readFileSync, writeFileSync } from "fs";
+import { promises as fs } from "fs";
 import { join } from "path";
-import { parse } from "../src/util/parser.util";
 
 describe("Format Reference Util", () => {
 
-    it("Format a reference with a DOI", () => {
-        const data: Reference[] = JSON.parse(readFileSync(join(__dirname, "assets", "test.json"), "utf-8"));
+    it("Format a reference with a DOI", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
 
         const result = formatReference(data[0]);
 
-        expect(result).toBe("[@Steinbeck2003](http://dx.doi.org/10.1021/ci025584y)");
+        expect(result).toBe("[@Steinbeck2005](http://dx.doi.org/10.1021/ci025584y)");
     });
 
-    it("Format a reference having a URL without a DOI", () => {
-        const data: Reference[] = JSON.parse(readFileSync(join(__dirname, "assets", "test.json"), "utf-8"));
-
+    it("Format a reference having a URL without a DOI", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
         const result = formatReference(data[1]);
 
         expect(result).toBe("[@Bu2010](http://www.aclweb.org/anthology/D10-1109)");
     });
 
-    it("Format a reference with neither URL nor DOI", () => {
-        const data: Reference[] = parse(readFileSync(join(__dirname, "assets", "test.bib"), "utf-8"));
+    it("Format a reference with neither URL nor DOI", async () => {
+        const data: Reference[] = JSON.parse(
+            await fs.readFile(join(__dirname, "assets", "test.json"), "utf-8")
+        );
 
         const result = formatReference(data[2]);
         
