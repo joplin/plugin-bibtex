4cf0edaa4f36c8bcd2f8b1d6ec2ca92926076142
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-06-22 13:20:36 +0200

    fix(Format Reference Util): Handle the cases where there is no author or year

--- src/util/format-ref.util.ts
+++ src/util/format-ref.util.ts
@@ -5,14 +5,46 @@ import { getDate } from "./get-date.util";
  * Format a given reference as a valid markdown url
  */
 export function formatReference (ref: Reference): string {
-    const author: string = ref.author[0].family;
-    const year: number = getDate(ref).getFullYear();
-    const title: string = ref.title.split(" ").join("+");
+    const displayText: string = getDisplayText(ref);
+    const link: string = getLink(ref);
+    return `[@${displayText}](${link})`;
+}
 
+/**
+ * Given a reference, returns the family name of the first author
+ * If there are no authors provided, returns empty string ("")
+ */
+function getDisplayText (ref: Reference): string {
+    const author = getAuthor(ref);
+    let text: string = author;
+    try {
+        text += getDate(ref).getFullYear();
+    } catch (e) { }
+
+    // If there is no author or year, put the title of the ref
+    if (author === "") text = ref.title.split(" ").join();
+    return text;
+}
+
+/**
+ * returns the url of the ref to be used in markdown
+ */
+function getLink (ref: Reference): string {
     let url: string = "";
-    if (ref.DOI && ref.DOI !== "") url = `http://dx.doi.org/${ref.DOI}`;
-    else if (ref.URL && ref.URL !== "") url = ref.URL;
-    else url = `https://scholar.google.com/scholar?q=${title},+${author}`;
-    
-    return `[@${author + year}](${url})`;
+    if (ref.DOI && ref.DOI !== "") {
+        url = `http://dx.doi.org/${ref.DOI}`;
+    } else if (ref.URL && ref.URL !== "") {
+        url = ref.URL;
+    } else {
+        url = `https://scholar.google.com/scholar?q=${ref.title}`;
+        if (getAuthor(ref) !== "") url += `, ${getAuthor(ref)}`;
+    }
+    return url;
+
+}
+
+function getAuthor (ref: Reference): string {
+    return (ref.author && ref.author.length > 0) ?
+        (ref.author[0].family) :
+        ("");
 }
