1f526711be322ee0cb61ea4fef044d75e10fa0ac
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-07-08 13:28:53 +0200

    fix(Format Reference Util): Use id as the display text

--- test/format-ref.test.ts
+++ test/format-ref.test.ts
@@ -12,7 +12,7 @@ describe("Format Reference Util", () => {
 
         const result = formatReference(data[0]);
 
-        expect(result).toBe("[@Steinbeck2005](http://dx.doi.org/10.1021%2Fci025584y)");
+        expect(result).toBe("[@Steinbeck2003](http://dx.doi.org/10.1021%2Fci025584y)");
     });
 
     it("Format a reference having a URL without a DOI", async () => {
@@ -21,7 +21,7 @@ describe("Format Reference Util", () => {
         );
         const result = formatReference(data[1]);
 
-        expect(result).toBe("[@Bu2010](http://www.aclweb.org/anthology/D10-1109)");
+        expect(result).toBe("[@bu-EtAl:2010:EMNLP](http://www.aclweb.org/anthology/D10-1109)");
     });
 
     it("Format a reference with neither URL nor DOI", async () => {
@@ -31,7 +31,7 @@ describe("Format Reference Util", () => {
 
         const result = formatReference(data[2]);
         
-        expect(result).toBe("[@Smith2005](https://scholar.google.com/scholar?q=MedTag%3A%20A%20Collection%20of%20Biomedical%20Annotations%2C%20Smith)");
+        expect(result).toBe("[@smith-EtAl:2005:Biolink](https://scholar.google.com/scholar?q=MedTag%3A%20A%20Collection%20of%20Biomedical%20Annotations%2C%20Smith)");
     });
 
 });
