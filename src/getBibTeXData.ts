de789472f7a39802613eb018d38c5d5990cf97aa
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-07-28 14:14:46 +0200

    refactor: Rename "bibtex file" settings field

--- src/getBibTeXData.ts
+++ src/getBibTeXData.ts
@@ -4,7 +4,7 @@ import { parse } from "./util/parser.util";
 import { Reference } from "./model/reference.model";
 import {
     ERROR_PARSING_FAILED,
-    SETTINGS_FILE_PATH_ID
+    SETTINGS_BIBTEX_FILE_PATH_ID,
 } from "./constants";
 const fs = joplin.require("fs-extra");
 
@@ -14,26 +14,26 @@ const fs = joplin.require("fs-extra");
  * Parse
  * Store
  */
-export async function getBibTeXData (): Promise<Reference[]> {
-
+export async function getBibTeXData(): Promise<Reference[]> {
     // Get file Paths
-    const filePaths: string[] = (await joplin.settings.value(SETTINGS_FILE_PATH_ID))
-        .split(";")                     // use ; as delimiter
-        .map(path => path.trim());      // remove spaces at the end of each path
+    const filePaths: string[] = (
+        await joplin.settings.value(SETTINGS_BIBTEX_FILE_PATH_ID)
+    )
+        .split(";") // use ; as delimiter
+        .map((path) => path.trim()); // remove spaces at the end of each path
 
-    // Read the contents of each file and 
+    // Read the contents of each file and
     let fileContent: string;
     try {
         fileContent = (
             await Promise.all(
-                filePaths.map(path => fs.readFile(path, "utf8"))
+                filePaths.map((path) => fs.readFile(path, "utf8"))
             )
         ).join("");
     } catch (e) {
         console.log(e);
         throw new Error(`Error: Could not open some files: ${e.message}`);
     }
-
 
     // Parse the raw data and store it
     let refs: Reference[] = [];
