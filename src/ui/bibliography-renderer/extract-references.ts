45138b4206af6ffdd491406d0083d287a2c03b29
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-08-11 02:57:00 +0200

    perf(Bibliography Renderer): Slight performance improvements

--- src/ui/bibliography-renderer/extract-references.ts
+++ src/ui/bibliography-renderer/extract-references.ts
@@ -5,31 +5,30 @@
  */
 export function extractReferences(tokens: any[]): string[] {
     const ids: string[] = [];
-    DFS(tokens, ids);
-    return ids;
-}
-
-function DFS(nodes: any[], ids): void {
-    /* Collect all words that matches the below regular expression */
     const referencePattern = /@(\w|:|\?|\-)+/g;
-    nodes.forEach((node) => {
-        if (
-            node["type"] === "text" &&
-            node.content &&
-            node.content.length > 1
-        ) {
-            /* A text token might contain several words separable by a space */
-            const content: string = node.content;
-            const matches = content.match(referencePattern);
-            if (matches && matches.length > 0) {
-                matches
-                    .map((word) => word.substring(1)) // Remove the @
-                    .forEach((word) => ids.push(word));
-            }
-        }
 
-        if (node["children"]) {
-            DFS(node["children"], ids);
-        }
-    });
+    /* Collect all words that matches the regular expression */
+    DFS(tokens);
+
+    function DFS(nodes: any[]): void {
+        nodes.forEach((node) => {
+            if (
+                node["type"] === "text" &&
+                node.content &&
+                node.content.length > 1
+            ) {
+                /* If matches found, add them to IDs without @ */
+                const matches = node.content.match(referencePattern);
+                if (matches && matches.length > 0) {
+                    matches.forEach((word) => ids.push(word.substring(1)));
+                }
+            }
+
+            if (node["children"]) {
+                DFS(node["children"]);
+            }
+        });
+    }
+
+    return ids;
 }
