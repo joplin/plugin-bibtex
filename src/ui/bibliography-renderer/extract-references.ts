dbab3be7b20d68082cd403d2e5c29bc4a5472ee0
Abdallah Ahmed <abdallahar1974@gmail.com>
2021-08-11 02:47:49 +0200

    feat(Bibliography Renderer): Extract all cite keys from the note body

--- src/ui/bibliography-renderer/extract-references.ts
+++ src/ui/bibliography-renderer/extract-references.ts
@@ -1,34 +1,35 @@
 /**
  * Given a list of markdown tokens and their children,
  * returns a list of reference IDs that exists in the markdown tree
- * Uses Depth-First-Search
+ * Uses recursive Depth-First-Search
  */
 export function extractReferences(tokens: any[]): string[] {
     const ids: string[] = [];
-    DFS(tokens);
+    DFS(tokens, ids);
+    return ids;
+}
 
-    /* Collect all words that starts with @ */
-    function DFS(nodes: any[]): void {
-        nodes.forEach((node) => {
-            if (
-                node["type"] === "text" &&
-                node.content &&
-                node.content.length > 1
-            ) {
-                /* A text token might contain several words separable by a space */
-                const content: string = node.content;
-                content
-                    .split(" ")
-                    .filter((word) => word.startsWith("@"))
+function DFS(nodes: any[], ids): void {
+    /* Collect all words that matches the below regular expression */
+    const referencePattern = /@(\w|:|\?|\-)+/g;
+    nodes.forEach((node) => {
+        if (
+            node["type"] === "text" &&
+            node.content &&
+            node.content.length > 1
+        ) {
+            /* A text token might contain several words separable by a space */
+            const content: string = node.content;
+            const matches = content.match(referencePattern);
+            if (matches && matches.length > 0) {
+                matches
                     .map((word) => word.substring(1)) // Remove the @
                     .forEach((word) => ids.push(word));
             }
+        }
 
-            if (node["children"]) {
-                DFS(node["children"]);
-            }
-        });
-    }
-
-    return ids;
+        if (node["children"]) {
+            DFS(node["children"], ids);
+        }
+    });
 }
